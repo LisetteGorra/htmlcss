5d3b5ea8ce1d86ad3762398316d98a8da8a6656f
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-05-01 19:09:42 -0400

    Array review, used .pop, .push, .shift

--- script5.js
+++ script5.js
@@ -4,5 +4,15 @@ console.log(fruits1);
 fruits1[1]="Pineapple";
 console.log(fruits1);
 
-fruits1.push("cranberry");
+fruits1.push("pumpkin");
 console.log(fruits1);
+
+fruits1.pop();
+console.log(fruits1);
+
+var fruits2 = ["kiwi", "oranges"];
+fruits2.push("sumo oranges");
+fruits2.push("grapes");
+fruits2.shift();
+fruits2[2] = "eggplants"
+console.log(fruits2);
