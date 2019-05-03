45871511da7e3bff08888694fc57addde0d768bf
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-05-02 20:49:25 -0400

    Created for loop for total length of array and console logged through array to get sum of each variable times array

--- script5.js
+++ script5.js
@@ -1,18 +1,31 @@
-var fruits1 = ["apple","peaches"];
+var fruits1 = ["apples","peaches"];
 console.log(fruits1);
 
-fruits1[1]="Pineapple";
+fruits1[1]="Pineapples";
 console.log(fruits1);
 
-fruits1.push("pumpkin");
+fruits1.push("pumpkins");
 console.log(fruits1);
 
 fruits1.pop();
 console.log(fruits1);
 
-var fruits2 = ["kiwi", "oranges"];
+var fruits2 = ["kiwis", "oranges"];
 fruits2.push("sumo oranges");
 fruits2.push("grapes");
 fruits2.shift();
-fruits2[2] = "eggplants"
+fruits2[2] = "eggplants";
 console.log(fruits2);
+var allfruits = fruits1.concat(fruits2);
+console.log(allfruits);
+
+var fruitprices = [1, 1.5, 2, 2.5, 3 ,4];
+
+var fruitsSold = [4, 10, 2, 6, 4, 16];
+
+
+for (var i = 0; i < allfruits.length; i++) {
+  
+    console.log("The total sales for", fruitsSold[i], allfruits[i], "is",fruitprices[i]*fruitsSold[i]);
+}
+
---
+++ script6.js
@@ -0,0 +1,38 @@
+var cars1 = ["Lexus", "Honda", "Acura"];
+var cars2 = ["Camaro","Audi","Yukon"];
+
+cars1[1]="BMW";
+console.log(cars1);
+cars2[1]="Skyline";
+console.log(cars2);
+cars1.push("Lambo");
+cars2.push("Nissan");
+console.log(cars1, cars2);
+cars1.unshift("Cadillac");
+cars2.unshift("Honda");
+console.log(cars1, cars2);
+cars1.pop();
+cars2.pop();
+console.log(cars1, cars2);
+cars2[2]="Lambo";
+console.log(cars2);
+
+var totalCars = cars1.concat(cars2);
+console.log(totalCars);
+
+var carsSold = [5,6,3,8,9,3,10,14];
+var prices = [50000, 64000, 65774, 76000, 78000];
+prices.push(87000, 92000, 100500);
+console.log(prices);
+
+for (var i = 0; i < totalCars.length; i++){
+    console.log("The", totalCars[i], "sold", carsSold[i], "times, for a total of", prices[i]*carsSold[i], "at", prices[i], "each!");
+}
+
+
+
+
+
+
+
+
