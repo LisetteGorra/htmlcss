032e1a037788302d781170bc254db70e9659fc90
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-05-04 11:21:43 -0400

    Cleaned up console.log, formatted whole interger in Volume to reflect with commas

--- stock.js
+++ stock.js
@@ -1,5 +1,5 @@
 var company = ["Zix Corporation ","Mercer International Inc.","Mercer International Inc.","China Internet Nationwide Financial Services Inc."];
-var stockSymbol = ["ZIXI", "MERC", "GEOS", "MELI", "CIFS"];
+var stockSymbol = ['ZIXI', "MERC", "GEOS", "MELI", "CIFS"];
 
 var sharePrices = [11.08, 17.40, 15.33, 578.94, 2.71];
 
@@ -8,5 +8,5 @@ var stockVolume = [6962846, 1421683, 135534, 2119134, 554787];
 for ( var i = 0; i < sharePrices.length; i++){
     var totalSold = stockVolume[i]*sharePrices[i];
 
-console.log(stockVolume[i], "shares of", stockSymbol[i], "traded yesterday at a price of ", sharePrices[i], "for a total of", totalSold.toLocaleString('en-US', {style: 'currency', currency: 'USD'}));
+console.log(stockVolume[i].toLocaleString(), "shares of", stockSymbol[i], "traded yesterday at a price of", sharePrices[i], "for a total of", totalSold.toLocaleString('en-US', {style: 'currency', currency: 'USD'}));
 }
